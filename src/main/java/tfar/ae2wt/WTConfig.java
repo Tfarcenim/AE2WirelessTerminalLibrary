aaacfcd33a1f27ecdbf55aa1a33edfc7d1923076
Mari023 <38946771+Mari023@users.noreply.github.com>
2021-06-01 23:55:02 +0200

    fix energy consumption being way too low out of range compared to the edge of in-range

--- src/main/java/tfar/ae2wt/WTConfig.java
+++ src/main/java/tfar/ae2wt/WTConfig.java
@@ -6,11 +6,11 @@ public class WTConfig {
 
     public static double getPowerMultiplier(double range, boolean isOutOfRange) {
         if(!isOutOfRange) return AEConfig.instance().wireless_getDrainRate(range);
-        return AEConfig.instance().wireless_getDrainRate(16 * getOutOfRangePowerMultiplier());
+        return AEConfig.instance().wireless_getDrainRate(528 * getOutOfRangePowerMultiplier());//the drain rate while in range is dependent on the distance, and calculated by ae2. since 528 blocks is the max distance a terminal can normally work, out of range has to calculate the power with max range, or otherwise it uses less energy when out of range than at the edge of range
     }
 
     public static double getChargeRate() {
-        return 32000;
+        return 32000;//8000 is ae2's default charge rate, so I chose to use it too
     }
 
     public static double WUTChargeRateMultiplier() {
@@ -18,6 +18,6 @@ public class WTConfig {
     }
 
     private static int getOutOfRangePowerMultiplier() {
-        return 2;
+        return 2;//has to be > 1 for terminals to use more energy outside of range than in range
     }
 }
